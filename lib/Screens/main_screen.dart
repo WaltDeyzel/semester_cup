61b07005a66e467d0c05cb048ba5793f7ed69781
Walt Deyzel <waltdeyzel3141@gmail.com>
2021-05-07 18:07:44 +0200

    Main screen frame is up.

---
+++ lib/Screens/main_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  
+  @override
+  _MainScreen createState() => _MainScreen();
+}
+
+class _MainScreen extends State<MainScreen> {
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      body: Text('Main Page') 
+    );
+  }
+}
