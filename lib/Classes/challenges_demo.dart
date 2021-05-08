3da5c0dbbe386ce33238fc4addd19fce51ba9dc8
Lynn Weyn <WYNLYN004@myuct.ac.za>
2021-05-08 12:47:26 +0200

    Made new list entries for the demo

--- lib/Classes/challenges_demo.dart
+++ lib/Classes/challenges_demo.dart
@@ -29,57 +29,78 @@ class ChallengeListDemo with ChangeNotifier {
             ),
           ),
           ChallengeEntry(
-      id: 'entry0',
-      challenge: Challenge(
-        id: 'c0',
-        title: 'table mountain',
-        description: 'Take a selfie with you and our favorite mountain!',
-      ),
-      pictureURL:
-          'https://cdn.getyourguide.com/img/location/546c82dc2cb7e.jpeg/88.jpg',
-      dateTime: DateTime.parse("1969-07-20 20:18:04Z"), // 8:18pm
-      votes: 10,
-      user: User(
-        name: 'Lynn',
-        studentNum: 'WYNLYN004',
-        email: 'WYNLYN004@myuct.ac.za',
-        points: 100,
-      ),
-    ),
-    ChallengeEntry(
-      id: 'entry2',
-      challenge: Challenge(
-        id: 'c2',
-        title: 'table mountain',
-        description: 'Take a selfie with you and our favorite mountain!',
-      ),
-      pictureURL:
-          'https://www.glenavon.co.za/wp-content/uploads/2017/12/Cape-Town-Attractions-Table-Mountain-View.jpg',
-      dateTime: DateTime.parse("1969-07-20 20:18:04Z"), // 8:18pm
-      votes: 10,
-      user: User(
-        name: 'Lynn',
-        studentNum: 'WYNLYN004',
-        email: 'WYNLYN004@myuct.ac.za',
-        points: 100,
-      ),
-    ),
+            id: 'entry0',
+            challenge: Challenge(
+              id: 'c0',
+              title: 'table mountain',
+              description: 'Take a selfie with you and our favorite mountain!',
+            ),
+            pictureURL:
+                'https://cdn.getyourguide.com/img/location/546c82dc2cb7e.jpeg/88.jpg',
+            dateTime: DateTime.parse("1969-07-20 20:18:04Z"), // 8:18pm
+            votes: 10,
+            user: User(
+              name: 'Lynn',
+              studentNum: 'WYNLYN004',
+              email: 'WYNLYN004@myuct.ac.za',
+              points: 100,
+            ),
+          ),
+          ChallengeEntry(
+            id: 'entry2',
+            challenge: Challenge(
+              id: 'c2',
+              title: 'table mountain',
+              description: 'Take a selfie with you and our favorite mountain!',
+            ),
+            pictureURL:
+                'https://www.glenavon.co.za/wp-content/uploads/2017/12/Cape-Town-Attractions-Table-Mountain-View.jpg',
+            dateTime: DateTime.parse("1969-07-20 20:18:04Z"), // 8:18pm
+            votes: 10,
+            user: User(
+              name: 'Lynn',
+              studentNum: 'WYNLYN004',
+              email: 'WYNLYN004@myuct.ac.za',
+              points: 100,
+            ),
+          ),
         ]),
     Challenge(
-      id: 'p2',
-      title: 'Run away from a UCT pigeon',
-      description: 'a UCT requirement. when visiting campus for your tut/exam.',
-      submits: []),
+        id: 'p2',
+        title: 'Run away from a UCT pigeon',
+        description:
+            'a UCT requirement. when visiting campus for your tut/exam.',
+        submits: []),
     Challenge(
-      id: 'p3',
-      title: 'Help Lynn',
-      description: 'Cause Walt wont do it for me .',
-      submits: []),
+        id: 'p5',
+        title: 'Get coffee from a vending machine',
+        description: 'with to much sugar.',
+        submits: []),
     Challenge(
-      id: 'p4',
-      title: 'Say "HECTIC" at the supermarket',
-      description: 'a UCT proverb.',
-      submits: [])
+        id: 'p6',
+        title: 'Feed a starling',
+        description: 'the UCT birdies.',
+        submits: []),
+    Challenge(
+        id: 'p4',
+        title: 'Blame Eduroam for all your problems',
+        description: 'the standard.',
+        submits: []),
+    Challenge(
+        id: 'p4',
+        title: 'Solve a Rubik\'s cube',
+        description: 'bragging rights.',
+        submits: []),
+    Challenge(
+        id: 'p4',
+        title: 'Attend a hackathon',
+        description: 'yay for devsoc!',
+        submits: []),
+    Challenge(
+        id: 'p4',
+        title: 'Get a UCT hoodie',
+        description: 'bragging rights.',
+        submits: []),
   ];
 
   List<Challenge> get items {
--- lib/Screens/spesific_challange_screen.dart
+++ lib/Screens/spesific_challange_screen.dart
@@ -14,24 +14,25 @@ class SpesificChallengeScreen extends StatefulWidget {
 class _SpesificChallengeScreen extends State<SpesificChallengeScreen> {
   File _storedImage;
   Future<void> _addChallengeEntry() async {
-    final imageFile = await ImagePicker.platform.pickImage(source: ImageSource.camera, maxWidth: 600);
+    final imageFile = await ImagePicker.platform
+        .pickImage(source: ImageSource.camera, maxWidth: 600);
   }
 
   @override
   Widget build(BuildContext context) {
     // Challenge challenge = ModalRoute.of(context).settings.arguments as Challenge;
-    Challenge selectedChallenge = ModalRoute.of(context).settings.arguments as Challenge;
+    Challenge selectedChallenge =
+        ModalRoute.of(context).settings.arguments as Challenge;
     List<ChallengeEntry> entries = selectedChallenge.items;
     return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
-      appBar: AppBar(
-        title: Text(selectedChallenge.title),
-      ),
-      body: ChallengeEntryGrid(entries),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add_a_photo),
-        onPressed: _addChallengeEntry,
-      ),
-    );
+        backgroundColor: Theme.of(context).primaryColor,
+        appBar: AppBar(
+          title: Text(selectedChallenge.title),
+        ),
+        body: ChallengeEntryGrid(entries),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add_a_photo),
+          onPressed: _addChallengeEntry,
+        ));
   }
 }
