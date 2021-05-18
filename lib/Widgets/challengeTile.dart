5ae08cd53f8b827eb1ea1cb3cc0461381ad5ba6a
Walt Deyzel <waltdeyzel3141@gmail.com>
2021-05-18 19:29:05 +0200

    Improved the look of the challanges screen.

---
+++ lib/Widgets/challengeTile.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class ChallangeTile extends StatefulWidget {
+  final selectedChallengeRoute;
+  final challange;
+  ChallangeTile(this.selectedChallengeRoute, this.challange);
+  @override
+  _ChallangeTile createState() =>
+      _ChallangeTile(this.selectedChallengeRoute, this.challange);
+}
+
+class _ChallangeTile extends State<ChallangeTile> {
+  final _selectedChallengeRoute;
+  final _challange;
+  _ChallangeTile(this._selectedChallengeRoute, this._challange);
+  bool info = false;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        _selectedChallengeRoute(_challange, context);
+      },
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(
+                    'https://cdn.mos.cms.futurecdn.net/ntFmJUZ8tw3ULD3tkBaAtf.jpg'),
+              ),
+            ),
+            child: Center(
+              child: info == false
+                  ? Text(_challange.title.toString(),
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24,
+                          color: Colors.black))
+                  : Card(
+                      margin: EdgeInsets.all(15),
+                      child: Text(
+                        _challange.description,
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+            ),
+            height: 300,
+            width: double.infinity,
+          ),
+          IconButton(
+              icon: Icon(Icons.info_outline),
+              onPressed: () {
+                setState(() {
+                  print(info);
+                  info = !info;
+                });
+              }),
+          Container(
+            margin: EdgeInsets.only(top: 285),
+            color: Color.fromRGBO(255, 255, 255, 0.75),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [Text('Entries: 100'), Text('Prize: R100')],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/Widgets/challenge_list.dart
+++ lib/Widgets/challenge_list.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:semester_cup/Screens/spesific_challange_screen.dart';
 import '../Classes/challenges_demo.dart';
 import '../Classes/challenge.dart';
+import '../Widgets/challengeTile.dart';
 
 class ChallengeList extends StatelessWidget {
   void selectedChallengeRoute(Challenge challenge, BuildContext context) {
@@ -14,31 +15,14 @@ class ChallengeList extends StatelessWidget {
   Widget build(BuildContext context) {
     final challenges = Provider.of<ChallengeListDemo>(context).items;
     return Container(
-      padding: EdgeInsets.all(10),
+      padding: const EdgeInsets.all(0),
       color: Theme.of(context).primaryColor,
       child: challenges.isEmpty
           ? Text('No Challenges')
           : Column(
               children: <Widget>[
-                ...challenges.map((element) {
-                  return InkWell(
-                    onTap: () {
-                      selectedChallengeRoute(element, context);
-                    },
-                    splashColor: Colors.blue,
-                    borderRadius: BorderRadius.circular(15),
-                    child: Card(
-                      color: Color.fromRGBO(141, 208, 252, 0.8),
-                      elevation: 1,
-                      child: ListTile(
-                        title: Text(
-                          element.title,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, color: Colors.white),
-                        ),
-                      ),
-                    ),
-                  );
+                ...challenges.map((challange) {
+                  return ChallangeTile(selectedChallengeRoute, challange);
                 }).toList(),
               ],
             ),
